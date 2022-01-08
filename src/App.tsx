d8851601f3e46a061173defbdee9bef0d281261e
Shai <smatsri@gmail.com>
2022-01-08 18:28:06 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+
 import './App.css';
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    const run = async () => {
+
+      const res = await axios.get('https://api.themoviedb.org/3/search/movie?api_key=9b7900cd166d37a6bbdefb32326ad272&query=big%20leb')
+      console.log(res.data);
+      
+    };
+    run();
+    
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
